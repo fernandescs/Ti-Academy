b777ccdb3ea11329fd4a0b3d2855a5a250d96c6d
Cleverton Fernandes <clevertoncsf@gmail.com>
2021-10-24 19:53:36 -0300

    Início das aulas Ciclo 4

---
+++ ciclo4/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { Menu } from './components/Menu'
+import { Home } from './views/Home'
+import { ListarClientes } from './views/Cliente/Listar'
+import { ListarPedidos  } from './views/Pedido/Listar'
+import { ListarServicos } from './views/Servico/Listar'
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Menu/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/listar-clientes" component={ListarClientes}/>
+          <Route path="/listar-pedidos" component={ListarPedidos}/>
+          <Route path="/listar-servicos" component={ListarServicos}/>
+
+
+        </Switch>
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ ciclo4/src/views/Servico/Listar/index.js
@@ -0,0 +1,7 @@
+export const ListarServicos = () =>{
+    return (
+        <div>
+            Listar Servicos
+        </div>
+    )
+}
